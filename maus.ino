b96f8eff8f75301c1670a6c573de1b2db37581e8
Joonas Laakkonen <joona@DESKTOP-H8751HD.localdomain>
2024-01-07 22:50:47 +0200

    comments and calibrationZ added in commented

--- maus.ino
+++ maus.ino
@@ -1,16 +1,19 @@
-#include <BleMouse.h>
+//Using the BleMouse library that functions similarly to the Mouse library. This way a bluetooth connection can be formed more easily.
 
+#include <BleMouse.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
-
+//Initializing the BLE device with the name "Maus". Battery percentage defaults to a constant value, as it is not measured by the device.
 BleMouse bleMouse("Maus", "Maus", 69);
-
+//Initialize the gyro/acc meter
 Adafruit_MPU6050 mpu;
 
+//Settings
 const int scanFrequency = 70; // Hz
-const int sensitivity = 100;
+const int sensitivity = 100; //Sensitivity adjustment. Sensitivity can also be adjusted inside the OS.
 
+//Constant values for pin numbers
 const int leftBtn = 18;
 const int rightBtn = 19;
 
@@ -24,17 +27,20 @@ const int calibrationThreshold = 5000; // ms, how long to hold both buttons for 
 
 int calibrationX = 0;
 int calibrationY = 0;
+//int calibrationZ = 0;
 
 void setup()
 {
+  //Set buttons as input
   pinMode(leftBtn, INPUT);
   pinMode(rightBtn, INPUT);
 
+  //Start monitoring serial for debug
   Serial.begin(115200);
 
   while (!Serial)
     delay(10);
-
+  //If mpu fails, indicate with serial
   if (!mpu.begin())
   {
     Serial.println("Failed to find MPU6050 chip");
@@ -43,8 +49,10 @@ void setup()
       delay(10);
     }
   }
+  //Otherwise mpu works
   Serial.println("MPU6050 Found!");
 
+  //Set MPU accelerometer range. Affects accuracy.
   mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
   Serial.print("Accelerometer range set to: ");
   switch (mpu.getAccelerometerRange())
@@ -62,7 +70,7 @@ void setup()
     Serial.println("+-16G");
     break;
   }
-
+  //Set MPU Gyro range
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   Serial.print("Gyro range set to: ");
   switch (mpu.getGyroRange())
@@ -81,6 +89,7 @@ void setup()
     break;
   }
 
+  //Set Filter bandwidth for MPU
   mpu.setFilterBandwidth(MPU6050_BAND_94_HZ);
   Serial.print("Filter bandwidth set to: ");
   switch (mpu.getFilterBandwidth())
@@ -107,10 +116,10 @@ void setup()
     Serial.println("5 Hz");
     break;
   }
-
+  
   Serial.println("");
   delay(500);
-
+  //Start BLE
   Serial.println("Starting BLE");
   bleMouse.begin();
 }
@@ -119,17 +128,19 @@ void loop()
 {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-
+  //Calibration
   double vx = g.gyro.z * sensitivity - calibrationX;
   double vy = g.gyro.y * sensitivity - calibrationY;
+  //double vz = g.gyro.x * sensitivity - calibrationZ;
 
+  //Print gyro values in serial for debug
   Serial.print("X: ");
   Serial.print(g.gyro.x);
   Serial.print(" Y: ");
   Serial.print(vx);
   Serial.print(" Z: ");
   Serial.println(vy);
-
+  //MAUS left button read
   if (digitalRead(leftBtn) == HIGH)
   {
     bleMouse.press(MOUSE_LEFT);
@@ -141,7 +152,7 @@ void loop()
     Serial.println("MOUSE LEFT CLICKED");
     endPressedL = millis();
   }
-
+  //MAUS right button read
   if (digitalRead(rightBtn) == HIGH)
   {
     bleMouse.press(MOUSE_RIGHT);
@@ -156,7 +167,7 @@ void loop()
 
   int holdTimeL = startPressedL - endPressedL;
   int holdTimeR = startPressedR - endPressedR;
-
+  //debug for calibration
   Serial.println(holdTimeL);
   Serial.println(holdTimeR);
 
@@ -165,8 +176,9 @@ void loop()
     Serial.println("CALIBRATING");
     calibrationX = vx;
     calibrationY = vy;
+    //calibrationZ = vz;
   }
-
+  //move mouse, delay for correct frequency
   bleMouse.move(vx, vy);
 
   delay(1000 / scanFrequency);
